ea169a60fb24c96cf07f01d7c261aed1c35df826
andriy <driga.and@gmail.com>
2024-05-14 14:10:37 +0300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const port = 8888;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+
---
+++ frontend/app/components/header/header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+    <header>
+        <h1>Music Shop</h1>
+    </header>
+    );
+}
+
+export default Header;
