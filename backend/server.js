102631fb0ceed9919a72a3bef89a906a3193c405
andriy <driga.and@gmail.com>
2024-06-13 18:09:47 +0300

    server deploy settings

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const app = express();
+require('dotenv').config(); 
 const PORT = 3001;
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: process.env.CLIENT_URL,
     credentials: true, 
 };
 
