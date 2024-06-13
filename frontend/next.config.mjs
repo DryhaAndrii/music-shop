30edbbb3772fa238a7822c91695200cba5bbc7cd
andriy <driga.and@gmail.com>
2024-06-13 17:26:01 +0300

    removed settings for netlify

--- frontend/next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,  // Включение режима строгого соблюдения правил React
-  swcMinify: true,
-};
-
-export default nextConfig;
