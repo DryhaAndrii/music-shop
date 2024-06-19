4e8a34d52b467994c79ae63dd856a4339067018e
andriy <driga.and@gmail.com>
2024-06-19 18:31:39 +0300

    admin panel add ability to add categories

--- backend/routes/adminRoutes/adminLoginRouter.js
+++ backend/routes/adminRoutes/adminLoginRouter.js
@@ -20,7 +20,7 @@ router.post('', async (req, res) => {
         console.log('login is nice, creating token');
 
         const payload = { id: admin.id, role: 'admin' };
-        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '5s' });
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
 
         console.log('created token, sending it to the client');
         res.cookie('token', token, { httpOnly: true, secure: true, sameSite: 'none', path: '/' });
--- backend/routes/adminRoutes/adminLogoutRouter.js
+++ backend/routes/adminRoutes/adminLogoutRouter.js
@@ -10,17 +10,10 @@ router.get('', (req, res) => {
         console.log('he has no token');
         return res.status(401).json({ message: 'You have no token', isToken: false });
     }
+    console.log('unlogging');
+    res.clearCookie("token");
+    return res.status(200).json({ message: 'Token is ok but you are logged out', isTokenValid: true });
 
-    try {
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-        console.log('deleting token to logout')
-        res.clearCookie("token");
-        return res.status(200).json({ message: 'Token is not valid', isTokenValid: false });
-    } catch (error) {
-        console.log('token is bad');
-        res.clearCookie("token");
-        return res.status(401).json({ message: 'Token is not valid', isTokenValid: false });
-    }
 });
 
 module.exports = router;
