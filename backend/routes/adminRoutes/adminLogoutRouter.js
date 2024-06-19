f1bca30e35f04c99b9833de13bae2380afe80024
andriy <driga.and@gmail.com>
2024-06-19 19:37:45 +0300

    admin panel add ability to add categories

--- backend/routes/adminRoutes/adminLogoutRouter.js
+++ backend/routes/adminRoutes/adminLogoutRouter.js
@@ -11,7 +11,7 @@ router.get('', (req, res) => {
         return res.status(401).json({ message: 'You have no token', isToken: false });
     }
     console.log('unlogging');
-    res.clearCookie("token");
+    res.clearCookie("token",{ httpOnly: true, secure: true, sameSite: 'none', path: '/' });
     return res.status(401).json({ message: 'Token is ok but you are logging out'  });
 
 });
