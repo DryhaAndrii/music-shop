c734b6275e6474118fcd41f78566f400fd1c9366
andriy <driga.and@gmail.com>
2024-07-22 19:48:55 +0300

    minor fix

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,6 @@
 // 'use client';
 
-import Consumer from "@/components/consumer/consumer";
-import Updater from "@/components/updater/updater";
-import ReduxProvider from "@/store/reduxProvider";
+
 
 import Slider from "@/components/slider/slider";
 
@@ -19,13 +17,7 @@ export default function Home() {
         <Slider />
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat maiores aperiam adipisci quasi ex repudiandae, corporis harum dolores libero nobis molestiae odit nostrum cupiditate. Incidunt, sapiente! Nihil rerum labore voluptatibus.
       </main>
-      {/* <ReduxProvider>
-
-        <Consumer />
-
-        <Updater />
-
-      </ReduxProvider> */}
+      
     </>
   );
 }
