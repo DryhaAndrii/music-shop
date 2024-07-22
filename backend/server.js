611476d67161691b1de8c5db773b41030cdb2975
andriy <driga.and@gmail.com>
2024-07-22 21:34:32 +0300

    fixed

--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,30 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-// const Grid = require('gridfs-stream');
-// const mongoose = require('mongoose');
 
 
+require('dotenv').config();
 
 const app = express();
 const PORT = 3001;
 
 const allowedOrigins = [
-    'https://music-shop-client.vercel.app',
-    'https://music-shop-admin-panel.netlify.app',
-    'http://localhost:3000',
-    'http://localhost:3000',
+    process.env.CLIENT_URL,
+    process.env.ADMIN_URL,
+    process.env.DEV_ADMIN_URL,
+    process.env.DEV_CLIENT_URL,
 ];
-
-console.log('CLIENT_URL:', process.env.CLIENT_URL);
-console.log('ADMIN_URL:', process.env.ADMIN_URL);
-console.log('DEV_ADMIN_URL:', process.env.DEV_ADMIN_URL);
-console.log('DEV_CLIENT_URL:', process.env.DEV_CLIENT_URL);
-
 
 const corsOptions = {
     origin: (origin, callback) => {
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
         } else {
-            console.log('Blocked by CORS:', origin); // Логируем заблокированные запросы
             callback(new Error('Not allowed by CORS'));
         }
     },
     credentials: true,
 };
-
-
-
-// const corsOptions = {
-//     origin: (origin, callback) => {
-//         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     },
-//     credentials: true,
-// };
 
 app.use(cors(corsOptions));
 app.use(express.json());
