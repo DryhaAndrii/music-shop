6c23339f81b9ad405defefb1a96772b9a48dfc5d
andriy <driga.and@gmail.com>
2024-07-22 21:06:38 +0300

    minor fix

--- frontend/components/header/headerComponents/catalogue/catalogueComponents/categories.tsx
+++ frontend/components/header/headerComponents/catalogue/catalogueComponents/categories.tsx
@@ -2,6 +2,7 @@ import MyButton from '@/components/myButton/myButton';
 import styles from '../styles.module.scss';
 import Link from 'next/link';
 import Category from '@/types/category';
+import CategoriesSkeleton from './categoriesSkeleton';
 interface CategoriesProps {
     show: boolean,
     categories: Category[] | undefined,
@@ -9,7 +10,10 @@ interface CategoriesProps {
 }
 
 function Categories({ show, categories, toggleMenu }: CategoriesProps) {
-    if(!show) return;
+    if (!show) return;
+    if (categories === undefined) return (
+        <CategoriesSkeleton />
+    )
     return (
         <div className={`${styles.categories}`} >
             <MyButton>
@@ -17,9 +21,12 @@ function Categories({ show, categories, toggleMenu }: CategoriesProps) {
             </MyButton>
             {
                 categories?.map((category, index) => (
+                    
                     <Link href={`/${category.title}`} className={styles.link} key={index}>
                         {category.title}
                     </Link>
+                    
+                    
                 ))
             }
         </div>
---
+++ frontend/components/header/headerComponents/catalogue/catalogueComponents/categoriesSkeleton.tsx
@@ -0,0 +1,17 @@
+
+import styles from '../styles.module.scss';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css'
+
+
+
+function CategoriesSkeleton() {
+
+    return (
+        <SkeletonTheme baseColor="#ffffff33" highlightColor="#ffffffa1" >
+            <Skeleton count={8} containerClassName={styles.categories} className={styles.link} style={{ width: '100px', height: '30px'}}/>
+        </SkeletonTheme>
+    );
+}
+
+export default CategoriesSkeleton;
